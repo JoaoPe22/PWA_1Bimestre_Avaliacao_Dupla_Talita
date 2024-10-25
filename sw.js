843321b597427ca42190b82e33d1546c0e938bae
JoaoPe22 <joao.canezin22@gmail.com>
2024-10-25 14:40:51 -0400

    commit

---
+++ sw.js
@@ -0,0 +1,18 @@
+let cacheName = "meus-horarios";
+let filesToCache = ["/", "/index.html", "/css/style.css", "/js/main.js", "/horarioAula.html", "/horarioPE.html"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
